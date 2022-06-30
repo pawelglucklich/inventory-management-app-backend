36260cc613e635c6ac5a4de056af194ec441357c
pawelglucklich <pawel.glucklich@gmail.com>
2022-06-30 15:12:24 +0200

    init commit - basic backend installation and configuration

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, {json} from "express";
+import cors from "cors";
+import 'express-async-errors';
+import rateLimit from "express-rate-limit";
+import { handleError } from "./utils/errors";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use(json());
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+}));
+
+// app.use('/', ...Router);
+
+app.use(handleError);
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on http://localhost:3001');
+});
