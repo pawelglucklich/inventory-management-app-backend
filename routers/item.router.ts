4819520d9cd278c7dd63ebf6cec86b0f8a22279f
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-07 14:43:08 +0200

    Added routes to router (get/search/article, get/id, post/)

--- routers/item.router.ts
+++ routers/item.router.ts
@@ -1,7 +1,20 @@
 import {Router} from "express";
+import {ItemRecord} from "../records/item.record";
 
 export const itemRouter = Router()
-    .get('/item', async (req, res) => {
-        console.log('Welcome to item!');
+    .get('/search/:article?', async (req, res) => {
+        const items = await ItemRecord.findAll(req.params.article ?? '');
+        res.json({items});
+    })
+
+    .get('/:id', async (req, res) => {
+        const item = await ItemRecord.getOne(req.params.id);
+        res.json(item);
+    })
+
+    .post('/', async (req, res) => {
+        const item = new ItemRecord(req.body);
+        await item.insert();
+        res.json(item);
     })
 ;
