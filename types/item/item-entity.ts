a9a5c3073849939d4ac0971bb741c7b571345396
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-07 14:43:47 +0200

    Added NewItemEntity to types.

--- types/item/item-entity.ts
+++ types/item/item-entity.ts
@@ -1,5 +1,10 @@
+export interface NewItemEntity extends Omit<ItemEntity, 'id'> {
+    id?: string;
+}
+
 export interface ItemEntity {
     id: string;
+    article: string;
     name: string;
     description?: string;
     quantity: number;
