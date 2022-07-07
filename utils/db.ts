19154e3b537c760e1c01b999cf8b9c47ac4fd2bf
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-07 14:45:41 +0200

    Added DB configuration.

---
+++ utils/db.ts
@@ -0,0 +1,9 @@
+import {createPool} from "mysql2/promise";
+
+export const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'megak_inventory_management',
+    namedPlaceholders: true,
+    decimalNumbers: true,
+});
